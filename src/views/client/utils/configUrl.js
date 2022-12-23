0b5c8f719781c9c21cec2b79cd4216579289a26f
kuluong2000 <tranhuuluong156200@gmail.com>
2022-12-23 16:35:04 +0700

    deploy

--- src/views/client/utils/configUrl.js
+++ src/views/client/utils/configUrl.js
@@ -1,2 +1,4 @@
-const BASE_URL = "http://127.0.0.1:3030/api";
+// const BASE_URL = "http://127.0.0.1:3030/api";
+const BASE_URL = 'https://doctorcare.up.railway.app/api';
+
 export default BASE_URL;
