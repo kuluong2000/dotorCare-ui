cc2ff87b087e9c5c7d42358f3f35fff16bfca50b
kuluong2000 <tranhuuluong156200@gmail.com>
2022-12-26 10:56:00 +0700

    x

--- src/views/client/utils/configUrl.js
+++ src/views/client/utils/configUrl.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://127.0.0.1:3030';
-// const BASE_URL = 'https://doctorcare.up.railway.app';
+// const BASE_URL = 'http://127.0.0.1:3030';
+const BASE_URL = 'https://doctorcare.up.railway.app';
 
 export default BASE_URL;
